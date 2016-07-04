fd3ecdecff4705dbd0a2e2fc02ce01930878b654
Roberta Doyle <ro.scalabrin@gmail.com>
2016-07-03 23:38:18 -0600

    added more feature tests

--- test/features/edit.rb
+++ test/features/edit.rb
@@ -3,16 +3,29 @@ require_relative "../test_helper"
 class EditTest < FeatureTest
 
   def test_it_can_edit_a_robot_info
+    robot_world.create({ :name => "R2D2", :city => "Denver", :state => "CO", :birthdate => "05/07/1987", :date_hired =>" 09/10/2004", :department => "sales"})
+    robot1 = robot_world.all[0]
+
     # as a user
     # When I go to the '/robots' page
     visit '/robots'
-    # I click on the edit icon bellow the first robot name
-    save_and_open_page
-    assert page.find('.robot-showcase-row:nth-of-type(1)').has_content?('R2D2')
-    # I should see "Welcome to Robot World" as the title of the page within a div
-    # assert page.find('div h1').has_content?("Welcome to Robot World")
-    # and I should see an undordered list with links to other pages
-    # assert page.find('body:nth-child(2)').has_css?("ul")
+    # I should see the robot name R2D2
+    assert page.find('.robot-showcase-row:nth-last-of-type(1)').has_content?('R2D2')
+    # and if I click on the edit icon bellow the robot name
+    page.find('.edit-delete button a').click
+    # I should get to to the edit page
+    assert_equal "/robots/#{robot1.id}/edit", current_path
+    # and if I fill in the name field with C3PO
+    within('.form-horizontal') do
+      page.find('#name-field').set('C3PO')
+    # and if I click the submit button
+      page.find('.btn-submit').click
+    end
+    # I should see the updated name on the index page
+    assert_equal "/robots", current_path
+    assert page.find('.robot-showcase-row:nth-last-of-type(1)').has_content?('C3PO')
+    # and I should not see the old name of the robot R2D2 on the index page
+    refute page.find('.robot-showcase-row:nth-last-of-type(1)').has_content?('R2D2')
   end
 
 end
--- test/features/index.rb
+++ test/features/index.rb
@@ -2,19 +2,43 @@ require_relative "../test_helper"
 
 class IndexTest < FeatureTest
 
-  def test_it_can_delete_a_robot_from_the_list
-    # as a user
-    # When I go to the '/robots' page
-    visit '/robots'
-  end
-
   def test_shows_the_robot_image_and_name
     # as a user
     # When I go to the '/robots' page
     visit '/robots'
   end
 
-  def test_there_is_an_icon_to_edit_a_robot_indo
+  def test_the_robot_name_link_takes_to_the_robot_page
+    robot_world.create({ :name => "R2D2", :city => "Denver", :state => "CO", :birthdate => "05/07/1987", :date_hired =>" 09/10/2004", :department => "sales"})
+    robot1 = robot_world.all[0]
+
+    # as a user
+    # When I go to the '/robots' page
+    visit '/robots'
+    # and I click on a robot name
+    page.find('.robot-showcase-row:nth-of-type(1)').click_on robot1.name
+    # I should be taken to the selected robot page
+    assert_equal "/robots/#{robot1.id}", current_path
+  end
+
+  def test_there_the_edit_icon_redirects_to_the_edit_page
+    robot_world.create({ :name => "R2D2", :city => "Denver", :state => "CO", :birthdate => "05/07/1987", :date_hired =>" 09/10/2004", :department => "sales"})
+    robot1 = robot_world.all[0]
+
+    # as a user
+    # When I go to the '/robots' page
+    visit '/robots'
+    # I click on the edit icon bellow the first robot name
+    # page.find('.robot-showcase-row:nth-of-type(1)').click_on robot1.name
+    within('.robot-showcase-row:nth-of-type(1)') do
+      assert page.has_content?('R2D2')
+      page.find('.edit-delete button a').click
+    end
+    # and I click on the idit icon bellow the robot name
+    assert_equal "/robots/#{robot1.id}/edit", current_path
+  end
+
+  def test_it_can_delete_a_robot_from_the_list
     # as a user
     # When I go to the '/robots' page
     visit '/robots'
